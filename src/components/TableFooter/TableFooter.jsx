3a9b97f43124f86846c09ac6163150b779c70b2b
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-03-17 16:44:52 +0100

    Add "Showing N of N entries" displayer

--- src/components/TableFooter/TableFooter.jsx
+++ src/components/TableFooter/TableFooter.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import "./TableFooter.css";
 
-export function TableFooter({ range, setCurrentPage, currentPage, slice }) {
+export function TableFooter({ range, setCurrentPage, currentPage, slice, orderedData, pageSize }) {
 
   useEffect(() => {
     if (slice.length < 1 && currentPage !== 1) {
@@ -9,8 +9,20 @@ export function TableFooter({ range, setCurrentPage, currentPage, slice }) {
     }
   }, [slice, currentPage, setCurrentPage]);
 
+  if ((pageSize * currentPage) > orderedData.length) console.log((currentPage * pageSize)-(pageSize - slice.length));
+
   return (
     <div className="tableFooter">
+      <span>
+        Showing {currentPage === 1 ? currentPage : ((currentPage-1) * pageSize) + 1} 
+        -{((pageSize * currentPage) > orderedData.length)
+        ? (currentPage * pageSize)-(pageSize - slice.length) 
+        : (currentPage * pageSize)
+        }
+        {' '} of {orderedData.length}{' '}
+        entries
+      </span>
+
       <button 
         className="tableFooter__btn" 
         disabled={currentPage === 1 ? true : false}
