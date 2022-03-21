5c92cb7863147f5469a4f5fb0c2c8f6a85a9d2c9
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-03-21 13:17:25 +0100

    Update sort icons style

--- src/components/TableHeader.jsx/TableHeader.jsx
+++ src/components/TableHeader.jsx/TableHeader.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import './TableHeader.css';
 
 TableHeader.propTypes = {
   headers: PropTypes.array,
@@ -20,7 +21,17 @@ export function TableHeader({ headers, ordering, setOrdering }) {
     <thead>
       <tr>
         {headers.map(header => {
-          return <th key={header.key} onClick={() => handleOrdering(header.key, ordering, setOrdering)}>{header.name}</th>
+          return (
+            <th key={header.key} onClick={() => handleOrdering(header.key, ordering, setOrdering)}>
+              <div className="th-content">
+                {header.name}
+                <span className='sortIcons'>
+                  <button className={(ordering && ordering.property === header.key && ordering.order === "ascending") ? "sortIcon--active" : null }>▲</button>
+                  <button className={(ordering && ordering.property === header.key && ordering.order === "descending") ? "sortIcon--active" : null }>▼</button>
+                </span>
+              </div>
+            </th>
+          )
         })}
       </tr>
     </thead>
@@ -31,9 +42,9 @@ function handleOrdering(key, ordering, setOrdering) {
   let newOrder;
   //Si la colonne est déjà triée
   if (ordering && ordering.property === key) {
-    newOrder = (ordering.order === 'ascending') 
-    ? { order: 'descending', property: key }
-    : { order: 'ascending', property: key }
+    newOrder = (ordering.order === 'ascending')
+      ? { order: 'descending', property: key }
+      : { order: 'ascending', property: key }
   }
   //Si la colonne n'est pas déjà triée
   else newOrder = { order: 'ascending', property: key }
