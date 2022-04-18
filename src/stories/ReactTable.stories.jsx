395dbc93c25c9f9e12e5bef543d5b9461d5b7ce5
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-04-18 17:19:13 +0200

    Add defaultSorting prop in stories

--- src/stories/ReactTable.stories.jsx
+++ src/stories/ReactTable.stories.jsx
@@ -8,5 +8,5 @@ import { default as ReactTable } from '../components/ReactTable.jsx';
 const stories = storiesOf('React Table', module);
 
 stories.add('Component', () => {
-  return (<ReactTable data={DATA} headers={HEADERS} />)
+  return (<ReactTable data={DATA} headers={HEADERS} defaultSorting={{ property: "firstName", order: "ascending" }} />)
 });
